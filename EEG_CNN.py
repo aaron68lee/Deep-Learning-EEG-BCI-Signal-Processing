a035c331eb57c210c37a3f744adf420355cc0ddc
Kevin1729 <kxia1729@gmail.com>
2024-03-06 13:35:41 -0800

    Deep gets 74% with preprocessing

--- EEG_CNN.py
+++ EEG_CNN.py
@@ -5,16 +5,9 @@ from keras.layers import Conv2D,BatchNormalization,MaxPooling2D,AveragePooling2D
 
 def CNN(reg=0, pool_size=3, use_max_pool=True, dropout=0.5, dropout_on_conv=False, filters=[10 for _ in range(4)], affine_layer_sizes=[], num_filters=[25, 50, 100, 200], input_shape=(500, 1, 22), output_classes=4):
     model = Sequential()
-    model.add(Conv2D(filters=num_filters[0], kernel_size=(filters[0],1), padding='same', activation='relu', input_shape=input_shape, kernel_regularizer=regularizers.L2(reg)))
-    if use_max_pool:
-        model.add(MaxPooling2D(pool_size=(pool_size,1), padding='same')) # Read the keras documentation
-    else:
-        model.add(AveragePooling2D(pool_size=(pool_size,1), padding='same')) # Read the keras documentation
-    model.add(BatchNormalization())
-    if dropout_on_conv:
-        model.add(Dropout(dropout))
-    for filter, num_filter in zip(filters[1:], num_filters[1:]):
-        model.add(Conv2D(filters=num_filter, kernel_size=(filter,1), padding='same', activation='relu', kernel_regularizer=regularizers.L2(reg)))
+    # Convolutional layers
+    for filter, num_filter in zip(filters, num_filters):
+        model.add(Conv2D(filters=num_filter, kernel_size=(filter,1), padding='same', activation='relu', input_shape=input_shape, kernel_regularizer=regularizers.L2(reg)))
         if use_max_pool:
             model.add(MaxPooling2D(pool_size=(pool_size,1), padding='same')) # Read the keras documentation
         else:
